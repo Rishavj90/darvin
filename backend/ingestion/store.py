13fcdf0b1997af26a2dbe6988bc8a3f46771db54
Rishav Jain <jainrishav632@gmail.com>
2026-06-06 16:12:17 +0530

    ingestion done

--- backend/ingestion/store.py
+++ backend/ingestion/store.py
@@ -1,11 +1,36 @@
 # 1. Load an embedding model
 # 2. Embed each chunk's text into a vector
 # 3. Store the vector + text + metadata into ChromaDB
-
-import chromadb
 from dotenv import load_dotenv
+from os import getenv
+import chromadb
+from sentence_transformers import SentenceTransformer
+
 
 load_dotenv()
+model = SentenceTransformer(getenv('EMBEDDING_MODEL'))
 
-def store_chunks(chunks_arr, chromadb_path=".../chromaDB"):
-    
+client = chromadb.PersistentClient(path=getenv("CHROMA_PATH"))
+collection = client.get_or_create_collection(
+    name=getenv("COLLECTION_NAME")
+)
+
+def store_chunks(chunks_arr):
+    id_arr=[]
+    txt_arr=[]
+    embeddings_arr=[]
+    metadata_arr=[]
+    for chunk in chunks_arr:
+        id_arr.append(chunk["chunk_id"])
+        txt_arr.append(chunk["text"])
+        embeddings_arr.append(model.encode(chunk["text"]).tolist())
+        metadata_arr.append({
+            "page" : chunk["page"],
+            "source" : chunk["source"]
+        })
+    collection.upsert(
+        ids=id_arr,
+        embeddings=embeddings_arr,
+        documents=txt_arr,
+        metadatas=metadata_arr
+    )
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import backend.ingestion.parse as ps
 import backend.ingestion.chunk as chnk
+import backend.ingestion.store as store
 
 arr = ps.parse_pdf("/home/rishav/Downloads/hello.pdf")
 chunks = chnk.chunk_pages(arr)
-print(f"total no. of chunks : {len(chunks)}")
-print(chunks[0])
+store.store_chunks(chunks)
+print("stored successfully :)")
